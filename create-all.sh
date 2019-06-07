108a55474710e134ddab39ba9dea2c7fb8368330
Lars Kellogg-Stedman <lars@redhat.com>
2019-06-07 15:49:00 -0400

    add some informational output to create-all.sh

--- create-all.sh
+++ create-all.sh
@@ -5,6 +5,7 @@ set -e
 : ${KEYSTONE_PUBLIC_HOSTNAME:="${USER}-keystone-dev.k-apps.osh.massopen.cloud"}
 CONFIGMAPS="httpd-config keystone-config keystone-templates keystone-vars"
 
+echo "creating configmaps"
 for x in $CONFIGMAPS; do
 	echo "creating configmap $x"
 
@@ -17,12 +18,20 @@ for x in $CONFIGMAPS; do
 	fi
 done
 
+echo "creating resources from template"
 oc delete all --all
 oc process -f flocx-keystone-dev.yaml -p KEYSTONE_PUBLIC_HOSTNAME=$KEYSTONE_PUBLIC_HOSTNAME | oc create -f-
 
+echo "waiting for keystone pod to become ready"
 while ! oc get -o json pod keystone | jq '.status.containerStatuses|.[].ready' | head -1 | grep -q true; do
-	echo "waiting for keystone pod"
+       sleep 1
+done
+
+echo "getting clouds.yaml"
+oc exec keystone cat /data/clouds.yaml > clouds.yaml
+
+echo "waiting for keystone api"
+while ! curl -sfk --connect-timeout 10 https://$KEYSTONE_PUBLIC_HOSTNAME -o /dev/null; do
 	sleep 1
 done
 
-oc exec keystone cat /data/clouds.yaml > clouds.yaml
